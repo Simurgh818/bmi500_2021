a0511ae8b9ea387c738a2f3e97c68079372bbac3
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-11-15 10:07:02 -0500

    2nd method for fs calculation. give fs as 125 instead of 120.

--- part_one/preprocess_marker_data.m
+++ part_one/preprocess_marker_data.m
@@ -2,9 +2,11 @@ function [filtered_data] = preprocess_marker_data(raw_data,trc_time, range)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 % number of samples per second
-fs = find(trc_time==1)-find(trc_time==0);
 
+% fs = find(trc_time==1)-find(trc_time==0);
+fs=1/(trc_time(2)-trc_time(1));
 fc= range;
+
 % normalized frequency
 w0 = fc/(fs/2);
 
